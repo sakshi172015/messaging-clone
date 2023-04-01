c96a96514dbd6c6a4c73f37b1705a76a0af096a5
Sakshi Jain <sakshi.172015@gmail.com>
2023-04-01 15:13:53 +0530

    cross origin policy

--- login-service/app.js
+++ login-service/app.js
@@ -2,10 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRouter from "./routes/user-routes";
-
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
+const options = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200,
+    methods: 'GET, PUT, POST, DELETE',
+  }
+app.use(cors(options))
 app.use(express.json())
 
 app.use('/api/user', userRouter)
--- message-service/app.js
+++ message-service/app.js
@@ -2,16 +2,25 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import messageRouter from "./routes/message-router";
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
+const options = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionSuccessStatus: 200,
+  methods: 'GET, PUT, POST, DELETE',
+}
+app.use(cors(options))
 app.use(express.json())
+
 app.use('/api/message', messageRouter)
 
 mongoose.set('strictQuery', true)
 mongoose
     .connect(process.env.MONGO_DB_CONNECTION_URL)
-    .then(() => app.listen(6000))
-    .then(() => console.log("PORT 6000 connected"))
+    .then(() => app.listen(9000))
+    .then(() => console.log("PORT 9000 connected"))
     .catch((err) => console.log(err))
