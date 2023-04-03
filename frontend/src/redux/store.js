d62c29edcc2a523c08d3463cc86891459cbcc5d9
Sakshi Jain <sakshi.172015@gmail.com>
2023-04-03 11:47:36 +0530

    activefriend

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from '../redux/userSlice'
+import activeFriendReducer from './activeFriendSlice'
 
 export default configureStore({
   	reducer: {
-    	user: userReducer
+    	user: userReducer,
+		activeFriend: activeFriendReducer
   	},
 })
